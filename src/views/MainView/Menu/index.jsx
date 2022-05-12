cfda7dd7532d183bb2b1d29e78e329b75c2c192a
Jimmy Torres <yimmytor@yahoo.es>
2022-05-11 23:52:44 -0600

    Layout

--- src/views/MainView/Menu/index.jsx
+++ src/views/MainView/Menu/index.jsx
@@ -2,7 +2,7 @@ import MenuOption from "./MenuOption";
 
 const Menu = () => {
     return(
-        <div>
+        <div className="grid grid-cols-2 gap-5">
             <MenuOption path="/products" title="Productos"/>            
             <MenuOption path="/stores" title="Comercios"/>            
             <MenuOption path="/shopping-list" title="Lista de Compras"/>            
--- src/views/MainView/index.jsx
+++ src/views/MainView/index.jsx
@@ -2,7 +2,8 @@ import Menu from "./Menu";
 
 const MainView = () => {
     return(
-        <main>
+        <main className="flex flex-col items-center justify-center
+                         h-screen bg-gradient-to-b from-sky-600 to-violet-900 ">
             <Menu />
         </main>
     );
